b86ca38a39cb4aaf2cb7215269686fc7447a18b0
Harry van Haaren <harryhaaren@gmail.com>
2018-06-30 04:30:26 +0100

    mappa: POC working with virtual device + crossfade
    This commit fixes a bug in the CtlraReader mappa instance,
    and registers a target for the crossfader in Mappa. The
    control over the crossfader target is mapped by the config
    file "mixxx_z1.ini".
    
    As of this commit, we can collaborate on mappings for the Z1,
    test them with virtual devices (if you don't have access to
    the hardware). This is the milestone that we've worked towards
    since December of '16!
    
    Previous PR available here: https://github.com/mixxxdj/mixxx/pull/1308/
    
    Note that this new approach has a significantly different design
    from the application side - using "Mappa" to perform the binding
    of hardware controls to software targets. Hence, it does not rely
    on TCC or JavaScript or ... it just loads a human-readable config
    file.
    
    The config file itself has enough power to enable users create complex
    and powerful multi-layered mappings - without the application (Mixxx)
    or the device (all hardware devices supported by Ctlra) to know about it.
    
    Signed-off-by: Harry van Haaren <harryhaaren@gmail.com>

--- src/controllers/ctlra/ctlraenumerator.cpp
+++ src/controllers/ctlra/ctlraenumerator.cpp
@@ -15,8 +15,9 @@
 #include <ctlra/event.h>
 #include <ctlra/mappa.h>
 
-CtlraReader::CtlraReader(struct mappa_t *c)
-	: QThread()
+CtlraReader::CtlraReader(struct mappa_t *m)
+	: QThread(),
+	mappa(m)
 {
 }
 
@@ -54,24 +55,46 @@ CtlraEnumerator::~CtlraEnumerator()
 	mappa_destroy(m_mappa);
 }
 
+static void
+mixxx_mappa_test_func(uint32_t target_id, float value, void *token,
+		      uint32_t token_size, void *userdata)
+{
+	printf("%s\n", __func__);
+	float v = (value * 2) - 1;
+	ControlProxy("[Master]", "crossfader").set(v);
+}
+
 QList<Controller*> CtlraEnumerator::queryDevices()
 {
 	m_mappa = mappa_create(NULL);
 	if(m_mappa == 0) {
 		printf("Ctlra error creating context!\n");
-		goto ret;
+		return m_devices;
 	}
+
+	struct mappa_target_t t = {0};
+	t.name = "mixxx_crossfader";
+	t.func = mixxx_mappa_test_func;
+	t.userdata = this;
+	uint32_t tid;
+	int ret = mappa_target_add(m_mappa, &t, &tid, 0, 0);
+	qDebug() << "CTLRA MIXXX TARGET REG returns " << ret;
+
+	printf("load bindings\n");
+	ret = mappa_load_bindings(m_mappa, "mixxx_z1.ini");
+	if(ret)
+		printf("%s %d: load bindings failed, ret %d\n",
+		       __func__, __LINE__, ret);
 
 	m_reader = new CtlraReader(m_mappa);
 	if(m_reader == nullptr) {
 		printf("CtlraEnumerator error creating m_reader!\n");
-		goto ret;
+		return m_devices;
 	}
 
 	// Controller input needs to be prioritized since it can affect the
 	// audio directly, like when scratching
 	m_reader->start(QThread::HighPriority);
 
-ret:
 	return m_devices;
 }
